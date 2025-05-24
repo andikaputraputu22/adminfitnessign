b7a45f9fd24b9ab9c3f34fb20eb52eba95a6db35
I Putu Andika Putra <andikaputra@detik.com>
2025-05-24 20:16:51 +0700

    Add instructor section

--- resources/views/instructors/create.blade.php
+++ resources/views/instructors/create.blade.php
@@ -3,67 +3,142 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <div class="card card-primary">
+                <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Complete All Forms Below</h3>
+                        <ul class="nav nav-pills">
+                            <li class="nav-item"><a class="nav-link active" href="#private_class" data-toggle="tab">Private
+                                    Class</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#instructor_class"
+                                    data-toggle="tab">Instructor Class</a></li>
+                        </ul>
                     </div>
-                    <form action="/instructors/store" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="file-dnd">
-                                        <label for="photo">Upload Photo</label>
-                                        <input type="file" id="photo" name="photo">
-                                        <div class="before-upload">
-                                            <div>
-                                                <i class="fa fa-image"></i>
-                                                <h4>Drag & Drop Image File or Browse</h4>
-                                                <p>Supports: JPEG, PNG, GIF, TIFF</p>
+                    <div class="card-body">
+                        <div class="tab-content">
+                            <!-- Private Class -->
+                            <div class="tab-pane active" id="private_class">
+                                <form action="/instructors/store" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="file-dnd">
+                                                <label for="photo">Upload Photo</label>
+                                                <input type="file" id="photo" name="photo">
+                                                <div class="before-upload">
+                                                    <div>
+                                                        <i class="fa fa-image"></i>
+                                                        <h4>Drag & Drop Image File or Browse</h4>
+                                                        <p>Supports: JPEG, PNG, GIF, TIFF</p>
+                                                    </div>
+                                                </div>
+                                                <div class="after-upload">
+                                                    <div class="clear-btn">&times;</div>
+                                                    <img src="" />
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="after-upload">
-                                            <div class="clear-btn">&times;</div>
-                                            <img src="" />
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="name">Name</label>
+                                                <input required type="text" class="form-control" id="name"
+                                                    name="name" placeholder="Enter name">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="email">Email Address</label>
+                                                <input required type="email" class="form-control" id="email"
+                                                    name="email" placeholder="Enter email">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="phone">Phone</label>
+                                                <input required type="text" class="form-control" id="phone"
+                                                    name="phone" placeholder="Enter phone">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="service">Select Service</label>
+                                                <select id="service" class="form-control" name="service_id[]" multiple
+                                                    size="5">
+                                                    @foreach ($services as $index => $service)
+                                                        <option value="{{ $service->id }}"
+                                                            {{ $index === 0 ? 'selected' : '' }}>{{ $service->name }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="name">Name</label>
-                                        <input required type="text" class="form-control" id="name" name="name"
-                                            placeholder="Enter name">
+                                        <label for="description">Description</label>
+                                        <input type="hidden" class="form-control" id="description" name="description">
+                                        <trix-editor input="description"></trix-editor>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="email">Email Address</label>
-                                        <input required type="email" class="form-control" id="email" name="email"
-                                            placeholder="Enter email">
+                                    <div class="text-right">
+                                        <button type="submit" class="btn btn-primary">Save</button>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="phone">Phone</label>
-                                        <input required type="text" class="form-control" id="phone" name="phone"
-                                            placeholder="Enter phone">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="service">Select Service</label>
-                                        <select id="service" class="form-control" name="service_id[]" multiple size="5">
-                                            @foreach ($services as $index => $service)
-                                                <option value="{{ $service->id }}" {{ $index === 0 ? 'selected' : '' }}>{{ $service->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                </div>
+                                </form>
                             </div>
-                            <div class="form-group">
-                                <label for="description">Description</label>
-                                <input type="hidden" class="form-control" id="description" name="description">
-                                <trix-editor input="description"></trix-editor>
+                            
+                            <!-- Instructor Class -->
+                            <div class="tab-pane" id="instructor_class">
+                                <form action="/instructors/store" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="file-dnd">
+                                                <label for="photo">Upload Photo</label>
+                                                <input type="file" id="photo" name="photo">
+                                                <div class="before-upload">
+                                                    <div>
+                                                        <i class="fa fa-image"></i>
+                                                        <h4>Drag & Drop Image File or Browse</h4>
+                                                        <p>Supports: JPEG, PNG, GIF, TIFF</p>
+                                                    </div>
+                                                </div>
+                                                <div class="after-upload">
+                                                    <div class="clear-btn">&times;</div>
+                                                    <img src="" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for="name">Name</label>
+                                                <input required type="text" class="form-control" id="name"
+                                                    name="name" placeholder="Enter name">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="email">Email Address</label>
+                                                <input required type="email" class="form-control" id="email"
+                                                    name="email" placeholder="Enter email">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="phone">Phone</label>
+                                                <input required type="text" class="form-control" id="phone"
+                                                    name="phone" placeholder="Enter phone">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="service">Select Service</label>
+                                                <select id="service" class="form-control" name="service_id[]" multiple
+                                                    size="5">
+                                                    @foreach ($services as $index => $service)
+                                                        <option value="{{ $service->id }}"
+                                                            {{ $index === 0 ? 'selected' : '' }}>{{ $service->name }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="description">Description</label>
+                                        <input type="hidden" class="form-control" id="description" name="description">
+                                        <trix-editor input="description"></trix-editor>
+                                    </div>
+                                    <div class="text-right">
+                                        <button type="submit" class="btn btn-primary">Save</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-                        <div class="card-footer text-right">
-                            <button type="submit" class="btn btn-primary">Save</button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
