282a77af59e928989deb53029df6e65b2f1d32c0
rudi <rudi.rezpone@gmail.com>
2025-08-14 14:19:08 +0700

    my current work

--- resources/views/components/frontend/frontend-hero.blade.php
+++ resources/views/components/frontend/frontend-hero.blade.php
@@ -1,7 +1,7 @@
 <section id="hero" class="hero section dark-background">
     <img src="frontend/assets/img/hero-bg-gym.jpg" alt="" data-aos="fade-in">
     <div class="container d-flex flex-column align-items-center">
-        <h2 data-aos="fade-up" data-aos-delay="100">TRAIN. COACH. TRANSFORM.</h2>
+        <h2 data-aos="fade-up" data-aos-delay="100">TRAIN .  COACH  . TRANSFORM</h2>
         <p data-aos="fade-up" data-aos-delay="200">We are a team of experienced instructors helping you achieve your fitness goals</p>
         <div class="d-flex mt-4" data-aos="fade-up" data-aos-delay="300">
             <a href="#about" class="btn-get-started">Get Started</a>
--- resources/views/components/frontend/frontend-instructor.blade.php
+++ resources/views/components/frontend/frontend-instructor.blade.php
@@ -1,16 +1,17 @@
 <section id="team" class="team section light-background">
     <div class="container section-title" data-aos="fade-up">
-        <h2>Instructor</h2>
-        <p>CHECK OUR INSTRUCTOR</p>
+        <h2>PERSONAL TRAINING </h2>
+        <p>CHECK OUR PERSONAL TRAINING </p>
     </div>
     <div class="container">
         <div class="row gy-5">
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
                 <div class="member">
-                    <div class="pic"><img style="height: 420px; width: 100%; object-fit: cover;" src="/frontend/assets/img/team/instructor-1.jpg" class="img-fluid" alt=""></div>
+                    <div class="pic"><img style="height: 420px; width: 100%; object-fit: cover;" src="/frontend/assets/img/team/FOTO PT BACKGROUND.jpeg" class="img-fluid" alt=""></div>
                     <div class="member-info">
-                        <h4>I Putu Andika Putra</h4>
-                        <span>Chief Executive Officer</span>
+                        <h4>COACH ()</h4>
+                        <span>Certified :</span>
+                        <span> Specialist : </span>
                         <div class="social">
                             <a href=""><i class="bi bi-facebook"></i></a>
                             <a href=""><i class="bi bi-instagram"></i></a>
@@ -21,10 +22,11 @@
 
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
                 <div class="member">
-                    <div class="pic"><img style="height: 420px; width: 100%; object-fit: cover;" src="/frontend/assets/img/team/team-2.jpg" class="img-fluid" alt=""></div>
+                    <div class="pic"><img style="height: 420px; width: 100%; object-fit: cover;" src="/frontend/assets/img/team/PERSONAL TRAINER WANITA.jpg" class="img-fluid" alt=""></div>
                     <div class="member-info">
-                        <h4>Sarah Anastasya</h4>
-                        <span>Product Manager</span>
+                        <h4>COACH ()</h4>
+                        <span>Certified :</span>
+                        <span>Specialist :</span>
                         <div class="social">
                             <a href=""><i class="bi bi-facebook"></i></a>
                             <a href=""><i class="bi bi-instagram"></i></a>
@@ -35,10 +37,11 @@
 
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
                 <div class="member">
-                    <div class="pic"><img style="height: 420px; width: 100%; object-fit: cover;" src="/frontend/assets/img/team/team-3.jpg" class="img-fluid" alt=""></div>
+                    <div class="pic"><img style="height: 420px; width: 100%; object-fit: cover;" src="/frontend/assets/img/team/personal PT3.jpg" class="img-fluid" alt=""></div>
                     <div class="member-info">
-                        <h4>Bambang Pengkung</h4>
-                        <span>CTO</span>
+                        <h4>COACH ()</h4>
+                        <span>Certified :</span>
+                        <span>Specialist :</span>
                         <div class="social">
                             <a href=""><i class="bi bi-facebook"></i></a>
                             <a href=""><i class="bi bi-instagram"></i></a>
