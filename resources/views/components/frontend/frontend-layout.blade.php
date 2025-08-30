f5107e13863f6f34c505acb801e3c23c8842069f
rudi <rudi.rezpone@gmail.com>
2025-08-30 16:57:04 +0700

    Update main layout

--- resources/views/components/frontend/frontend-layout.blade.php
+++ resources/views/components/frontend/frontend-layout.blade.php
@@ -19,7 +19,7 @@
 
 </head>
 
-<body class="index-page" styles ="background-color2: #000000ff">
+<body class="index-page">
     <x-frontend.frontend-header></x-frontend.frontend-header>
     <main class="main">
         {{ $slot }}
