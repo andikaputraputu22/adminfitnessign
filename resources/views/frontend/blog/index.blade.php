ced8dff1e652759e62ddc54528c23eaee9af1336
I Putu Andika Putra <andikaputra@detik.com>
2025-08-30 20:34:36 +0700

    Adjustment header menu

---
+++ resources/views/frontend/blog/index.blade.php
@@ -0,0 +1,4 @@
+<x-frontend.frontend-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <x-frontend.blog></x-frontend.blog>
+</x-frontend.frontend-layout>
--- routes/frontend.php
+++ routes/frontend.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\BlogController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\InstructorController;
 use Illuminate\Support\Facades\Route;
@@ -7,3 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('frontend.home');
 
 Route::get('/instructors', [InstructorController::class, 'index'])->name('frontend.instructor');
+
+Route::get('/blog', [BlogController::class, 'index'])->name('frontend.blog');
