d62ce08ca872c043270ac2e90882b958d4e0d066
I Putu Andika Putra <andikaputra@detik.com>
2025-09-18 19:31:42 +0700

    Add about section

---
+++ resources/views/components/frontend/frontend-about.blade.php
@@ -0,0 +1,20 @@
+<section id="about" class="features section">
+    <div class="container section-title" data-aos="fade-up">
+        <h2>ABOUT US</h2>
+        <p>CHECK OUR INFO</p>
+    </div>
+    <div class="container">
+        <div data-aos="fade-up" data-aos-delay="200">
+            <div class="row">
+                <div class="col-lg-6 order-2 order-lg-1 d-flex align-items-center">
+                    <p class="about-text">
+                        Di Fitnessign, kami percaya bahwa olahraga adalah hak semua orang, dari usia prima hingga lansia. Karena itu, program kami dirancang agar mudah diikuti, menyenangkan, sekaligus efektif untuk menjaga tubuh tetap bugar dan bersemangat.
+                    </p>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2 d-flex justify-content-end">
+                    <img src="frontend/assets/img/about_us.jpg" alt="" class="about-img img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -2,4 +2,5 @@
     <x-slot:title>{{ $title }}</x-slot:title>
     <x-frontend.frontend-hero></x-frontend.frontend-hero>
     <x-frontend.frontend-instructor></x-frontend.frontend-instructor>
+    <x-frontend.frontend-about></x-frontend.frontend-about>
 </x-frontend.frontend-layout>
