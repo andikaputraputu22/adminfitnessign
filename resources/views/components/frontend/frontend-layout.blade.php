a37dabe0279b7de119d0e9a5568838e07baff521
Adli-naufal <adlinaufal13@gmail.com>
2025-10-02 19:25:14 +0700

    add dynamic copyright year in footer

--- resources/views/components/frontend/frontend-layout.blade.php
+++ resources/views/components/frontend/frontend-layout.blade.php
@@ -33,6 +33,8 @@
 
   <!-- Preloader -->
   <div id="preloader"></div>
+
+  
 </body>
 
 <script src="/frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -43,6 +45,12 @@
 <script src="/frontend/assets/vendor/swiper/swiper-bundle.min.js"></script>
 <script src="/frontend/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
 <script src="/frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+document.getElementById("year").innerText = new Date().getFullYear();
 <script src="/frontend/assets/js/main.js"></script>
+
+<!-- Dynamic year -->
+<script>
+  document.getElementById("year").textContent = new Date().getFullYear();
+</script>
 
 </html>
