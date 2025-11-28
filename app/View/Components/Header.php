9e498392e6728bc2bee9848ff814e8f09fb10e8b
I Putu Andika Putra <andikaputraputu22@gmail.com>
2025-11-28 17:35:27 +0700

    Fix view header not found

--- app/View/Components/Header.php
+++ app/View/Components/Header.php
@@ -21,6 +21,6 @@ class Header extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.frontend.header');
+        return view('components.header');
     }
 }
