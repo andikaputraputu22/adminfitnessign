3d03e535a6f41e87b0bf0b0f3cdcd3b557aad86b
Adli-naufal <adlinaufal13@gmail.com>
2025-12-21 21:42:28 +0700

    edit detail instructor

--- resources/views/components/frontend/detail-instructor-content.blade.php
+++ resources/views/components/frontend/detail-instructor-content.blade.php
@@ -1,16 +1,42 @@
-<section id="detailInstructor" class="team section fitnessign-light-background">
-    <div class="container">
-        <div data-aos="fade-up" data-aos-delay="200">
-            <div class="row">
-                <div class="col-lg-4 order-2 order-lg-1 d-flex justify-content-end">
-                    <img src="{{ asset($instructor->photo) }}" alt="" class="img-fluid about-img">
-                </div>
-                <div class="col-lg-8 order-1 order-lg-2 d-flex align-items-center">
-                    <p class="about-text">
-                        {{ strip_tags($instructor->description) }}
-                    </p>
-                </div>
-            </div>
+<section id="detailInstructor" class="section dark-background">
+  <div class="container">
+    <div class="row align-items-start">
+
+      <!-- LEFT: PHOTO + INFO -->
+      <div class="col-lg-4">
+        <div class="coach-card">
+          <img src="{{ asset('storage/'.$instructor->photo) }}" class="img-fluid rounded-4">
+
+          <h5 class="coach-name mt-3">{{ $instructor->name }}</h5>
+
+          <div class="coach-block">
+            <h6>Certified</h6>
+            {!! $instructor->certificate !!}
+          </div>
+
+          <div class="coach-block">
+            <h6>Specialist</h6>
+            {!! $instructor->specialist !!}
+          </div>
         </div>
+      </div>
+
+      <!-- RIGHT: CONTENT -->
+      <div class="col-lg-8">
+        <h2 class="coach-title">
+          FITNESSIGN PERSONAL TRAINING
+        </h2>
+
+        <div class="coach-description">
+          {!! $instructor->description !!}
+        </div>
+
+          <a href="https://wa.me/62XXXXXXXX" class="btn btn-success mt-3">
+            BOOK NOW
+          </a>
+        </div>
+      </div>
+
     </div>
+  </div>
 </section>
