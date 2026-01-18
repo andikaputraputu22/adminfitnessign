358de51efa773a51f1a494de9fca469347717466
I Putu Andika Putra <andikaputraputu22@gmail.com>
2026-01-18 21:29:25 +0700

    Show blog list

--- app/Http/Controllers/Frontend/BlogController.php
+++ app/Http/Controllers/Frontend/BlogController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function index() {
         return view('frontend.blog.index', [
-            'title' => 'Health Blog'
+            'title' => 'Health Blog',
+            'blogs' => Blog::all()
         ]);
     }
 }
