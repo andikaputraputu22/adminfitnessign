d644c23d4e0edc52e4925f4cd3fe90654b392819
Adli-naufal <adlinaufal13@gmail.com>
2026-01-22 14:37:58 +0700

    creating detail blog page

--- app/Http/Controllers/Frontend/BlogController.php
+++ app/Http/Controllers/Frontend/BlogController.php
@@ -3,13 +3,22 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('frontend.blog.index', [
-            'title' => 'Health Blog'
+            'title' => 'Health Blog',
+            'blogs' => Blog::latest()->get(),
+        ]);
+    }
+
+    public function show($slug)
+    {
+        return view('frontend.blog.show', [
+            'blog' => Blog::where('slug', $slug)->firstOrFail(),
         ]);
     }
 }
--- resources/views/frontend/blog/index.blade.php
+++ resources/views/frontend/blog/index.blade.php
@@ -1,5 +1,5 @@
 <x-frontend.frontend-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
     <x-frontend.blog-hero>{{ $title }}</x-frontend.blog-hero>
-    <x-frontend.blog-list></x-frontend.blog-list>
+    <x-frontend.blog-list :blogs="$blogs" />
 </x-frontend.frontend-layout>
