c844149ffcbd9b29346b03b291fc5a9d2b3ab4b4
I Putu Andika Putra <andikaputraputu22@gmail.com>
2026-01-22 22:47:25 +0700

    Adjustment method delete for instructor

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -64,9 +64,9 @@ class InstructorController extends Controller
     }
 
     public function delete($id) {
-        $instructor = Instructor::find($id);
-        if ($instructor->photo) {
-            Storage::delete($instructor->photo);
+        $instructor = Instructor::findOrFail($id);
+        if ($instructor->photo && Storage::disk('public')->exists($instructor->photo)) {
+            Storage::disk('public')->delete($instructor->photo);
         }
         $instructor->delete();
         return redirect()->back()->with('success', 'Instructor has been deleted');
