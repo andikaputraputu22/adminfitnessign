c32b81e3df65efc03c77b19941af1d93f5881d7e
Adli-naufal <adlinaufal13@gmail.com>
2026-02-07 19:29:52 +0700

    refactor(instructor): move intro text to pages and make instructor-card canonical adapter

--- resources/views/frontend/blog/index.blade.php
+++ resources/views/frontend/blog/index.blade.php
@@ -1,7 +1,7 @@
 <x-frontend.frontend-layout>
-    <x-slot:title>Blog</x-slot:title>
+    <x-slot:title>Health Blog</x-slot:title>
 
-    <x-frontend.blog-hero title="Blog" />
+    <x-frontend.blog-hero title="Health Blog" />
 
     <x-frontend.blog-list :blogs="$blogs" />
 </x-frontend.frontend-layout>
