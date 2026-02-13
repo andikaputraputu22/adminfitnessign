672d0893e4dc6af058dfed296506da214e65c080
Adli-naufal <adlinaufal13@gmail.com>
2026-02-13 21:50:24 +0700

    feat(frontend): fully service-driven instructor & trainer pages

--- resources/views/frontend/instructor/index.blade.php
+++ resources/views/frontend/instructor/index.blade.php
@@ -1,14 +1,17 @@
 <x-frontend.frontend-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
+    <x-slot:title>{{ $service->name }}</x-slot:title>
+
     <x-frontend.instructor-hero
-        :title="$title"
+        :title="$service->name"
         :image="$service->photo" />
 
-    {{-- LIST + INTRO MUST SHARE THE SAME SECTION --}}
     <section id="team" class="team section fitnessign-light-background">
-        <div class="container title-description text-justify mb-4" data-aos="fade-up">
-            <p>{{ $service->description }}</p>
-        </div>
+        @if($service->description)
+            <div class="container title-description text-justify mb-4" data-aos="fade-up">
+                <p>{{ $service->description }}</p>
+            </div>
+        @endif
+
         <x-instructor-grid :instructors="$instructors" />
     </section>
 </x-frontend.frontend-layout>
